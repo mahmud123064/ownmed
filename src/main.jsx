67df32a6e7d6b39abd12c8dd49a2651ba6f3d21c
Mahmud Uz Zaman <zmahmud26@gmail.com>
2026-01-18 00:01:40 +0600

    page layout setup done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import router from "../src/Router/routes"
+
+import {
+ 
+  RouterProvider,
+} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
