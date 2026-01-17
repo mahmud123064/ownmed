789f8a0bfcdfc81c69e230a71fb9f5e4df9676a8
Mahmud Uz Zaman <zmahmud26@gmail.com>
2026-01-17 21:33:39 +0600

    tesst

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+// import { Input } from "@/components/ui/input"
+
+function App() {
+ 
+
+  return (
+    <>
+      <p className='text-4xl text-blue-600'>hello</p>
+      <Button>Button</Button>
+      <Button variant="outline">Outline</Button>
+      {/* <Input type="email" placeholder="Email" /> */}
+    </>
+  )
+}
+
+export default App
