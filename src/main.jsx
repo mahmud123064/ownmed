2675ddeee9eb310bb40401a03b4ce82fb01d588c
Mahmud Uz Zaman <zmahmud26@gmail.com>
2026-02-07 09:46:52 +0600

    Cloudinary added for the image upload

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,5 @@ createRoot(document.getElementById("root")).render(
         <Provider store={store}>
             <RouterProvider router={router} />
         </Provider>
-        
     </StrictMode>,
 );
